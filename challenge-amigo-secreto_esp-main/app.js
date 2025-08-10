0e65baf8604035ae15b0a2095df1f9165ce4d2d2
Adrian Ramos <adrianr694@gmail.com>
2025-08-09 20:13:21 -0600

    añade función de mostrar lista y sortear amigos

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,6 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-
+//Lista de amigos en el documento html
+let lista = document.getElementById('listaAmigos');
+//resultado en el html
+let resultado = document.getElementById('resultado');
+//Arreglo con los numeros ingresados
 let listaNombres=[];
+//Variable para el numero aleatorio 
+let numGen = 0;
 //Funcion para cambiar texto en el archivo html
 function asignacionTexto(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
@@ -18,13 +24,15 @@ function agregarAmigo(){
     }else{
         listaNombres.push(nombreAmigo);
         console.log(listaNombres);
-        limpiarTexto('#amigo');        
+        limpiarTexto('#amigo');     
+        lista.innerHTML=''       
         mostrarLista();
+
     }
 
 }
 
-//Deja en blanco la caja del imput
+//Deja en blanco la caja del input
 function limpiarTexto(DocumentoID){
     let valorCaja = document.querySelector(DocumentoID);
     valorCaja.value = '';
@@ -32,8 +40,7 @@ function limpiarTexto(DocumentoID){
 }
 
 //Funcion para mostrar la lista de amigos
-function mostrarLista(){        
-    let lista = document.getElementById('listaAmigos');    
+function mostrarLista(){               
     //Muestra la lista
     for(i=0;i<listaNombres.length;i++){
        let li = document.createElement('li');
@@ -42,10 +49,18 @@ function mostrarLista(){
     }
 }
 
-function limpiarLista(){        
-    let lista = document.getElementById('listaAmigos');    
-    //Muestra la lista
-    for(i=0;i<listaNombres.length;i++){
-        lista.removeChild(lista.firstChild)
+// Sortea un nombre de la lista de manera aleatoria
+function sortearAmigo(){
+    //Limpia el resultado anterior
+    resultado.innerHTML = ''
+    //Verifica si hay nombres disponibles para sortear
+    if (listaNombres.length == 0){
+        alert('No hay nombres en la lista')
+    }else{
+        numGen = Math.floor(Math.random()*listaNombres.length); 
+        console.log(numGen);     
+        let li = document.createElement('li');
+        li.innerText = listaNombres[numGen];
+        resultado.appendChild(li);
     }
 }
